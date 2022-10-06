a563f25a03973853bad6eee8abe958ddfd97e600
Arturo <atercerosb@univalle.edu>
2022-10-06 17:30:09 -0400

    add styles modal

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,18 +1,15 @@
 import React from "react";
-import { ImCross } from "react-icons/im";
+
 import { ProfileCharacter } from "./ProfileCharacter";
 
 function Character({ setOpenModal, resetTurn, optionOne }) {
-  const onCancel = () => {
-    setOpenModal(false);
-    resetTurn();
-  };
   return (
-    <div>
-      <span className="todoForm-button__cancel" onClick={onCancel}>
-        <ProfileCharacter character={optionOne} />
-        <ImCross />{" "}
-      </span>
+    <div className="character-container">
+      <ProfileCharacter
+        character={optionOne}
+        setOpenModal={setOpenModal}
+        resetTurn={resetTurn}
+      />
     </div>
   );
 }
--- src/components/ProfileCharacter.jsx
+++ src/components/ProfileCharacter.jsx
@@ -1,13 +1,39 @@
 import React from "react";
+import { ImCross } from "react-icons/im";
 
-function ProfileCharacter({ character }) {
+function ProfileCharacter({ character, setOpenModal, resetTurn }) {
+  const onCancel = () => {
+    setOpenModal(false);
+    resetTurn();
+  };
   return (
-    <div>
-      <span>Nombre:</span>
-      <span>{character.name}</span>
-      <span>Especie:</span>
-      <span>{character.species}</span>
+    <div className="profile-container">
       <img src={character.image} alt={`${character.name}'s image`} />
+      <span>{character.name}</span>
+      <span>Info</span>
+      <ImCross className="close-button" onClick={onCancel} />
+      <div className="character-info">
+        <div>
+          <span>Especie:</span>
+          <span>{character.species}</span>
+        </div>
+        <div>
+          <span>Genero:</span>
+          <span>{character.gender}</span>
+        </div>
+        <div>
+          <span>Status:</span>
+          <span>{character.status}</span>
+        </div>
+        <div>
+          <span>Tipo:</span>
+          <span>{character.type}</span>
+        </div>
+        <div>
+          <span>Location: </span>
+          <span>{character.location.name}</span>
+        </div>
+      </div>
     </div>
   );
 }
